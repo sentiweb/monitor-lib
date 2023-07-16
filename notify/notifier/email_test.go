92090f8b42dd8867f5534b0696567dd91e32e11f
Clément Turbelin <clement.turbelin@gmail.com>
2023-07-16 13:25:25 +0200

    email notifier test

---
+++ notify/notifier/email_test.go
@@ -0,0 +1,43 @@
+package notifier
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/sentiweb/monitor-lib/notify/email"
+	"github.com/sentiweb/monitor-lib/notify/tests"
+	utils_tests "github.com/sentiweb/monitor-lib/tests"
+	"github.com/sentiweb/monitor-lib/utils"
+)
+
+func TestEmailNotifier(t *testing.T) {
+
+	defer utils_tests.CaptureLog(t).Release()
+
+	sender := email.NewMemorySender()
+
+	notifier := NewEmailNotifier(sender, []string{"me@example.com"}, "Notification", 0, nil)
+
+	context := context.WithValue(context.TODO(), utils.ContextDebug, true)
+
+	err := notifier.Start(context)
+	if err != nil {
+		t.Error(err)
+	}
+
+	fmt.Println(notifier)
+
+	notif := tests.NewMockNotification("up", "test12341", time.Now())
+
+	notifier.Send(context, notif)
+
+	aw := utils_tests.NewAwait(time.Second, 100*time.Millisecond)
+
+	check := aw.Wait(func() bool {
+		return len(sender.Messages()) == 1
+	})
+
+	t.Logf("Result %t, waited %s", check, aw.TimeWaited())
+}
