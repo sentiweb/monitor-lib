f31d5993823bf4fd8500a873c261a5f6ea129d92
Clément Turbelin <clement.turbelin@gmail.com>
2023-07-18 11:11:25 +0200

    periods can be serialized to text

--- period/period.go
+++ period/period.go
@@ -1,13 +1,15 @@
 package period
 
 import (
-	"time"
+	"fmt"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // Period describes a period element for which you can test if a value is included
 type Period interface {
+	fmt.Stringer
 	In(v int) bool
 }
 
@@ -18,6 +20,14 @@ type PeriodeValue struct {
 
 func (p PeriodeValue) In(v int) bool {
 	return p.Value == v
+}
+
+func (p PeriodeValue) String() string {
+	return fmt.Sprintf("%d", p.Value)
+}
+
+func (p PeriodeValue) MarshalText() (text []byte, err error) {
+	return []byte(p.String()), nil
 }
 
 // PeriodRange defines a range of values
@@ -31,9 +41,17 @@ func (p PeriodRange) In(v int) bool {
 	return v >= p.Min && v <= p.Max
 }
 
+func (p PeriodRange) String() string {
+	return fmt.Sprintf("%d-%d", p.Min, p.Max)
+}
+
+func (p PeriodRange) MarshalText() (text []byte, err error) {
+	return []byte(p.String()), nil
+}
+
 // PeriodDefinition  Defines criteria to match a ref time
 // Like a Cron period definition.
-type PeriodDefinition  struct {
+type PeriodDefinition struct {
 	Minutes []Period
 	Hours   []Period
 	Days    []Period
@@ -42,7 +60,7 @@ type PeriodDefinition  struct {
 }
 
 // In checks if ref time matches the period criteria
-func (p PeriodDefinition ) In(ref time.Time) bool {
+func (p *PeriodDefinition) In(ref time.Time) bool {
 	inside := true
 	if inside && len(p.Minutes) > 0 {
 		inside = inside && inValue(p.Minutes, ref.Minute())
@@ -62,6 +80,33 @@ func (p PeriodDefinition ) In(ref time.Time) bool {
 	return inside
 }
 
+func (p *PeriodDefinition) String() string {
+	min := periodsToText(p.Minutes)
+	h := periodsToText(p.Hours)
+	days := periodsToText(p.Days)
+	months := periodsToText(p.Months)
+	dows := periodsToText(p.Dows)
+	return fmt.Sprintf("%s %s %s %s %s", min, h, days, months, dows)
+}
+
+func (p *PeriodDefinition) MarshalText() (text []byte, err error) {
+	return []byte(p.String()), nil
+}
+
+func periodsToText(periods []Period) string {
+	if len(periods) == 0 {
+		return "*"
+	}
+	b := strings.Builder{}
+	for i, p := range periods {
+		if i > 0 {
+			b.WriteRune(',')
+		}
+		b.WriteString(p.String())
+	}
+	return b.String()
+}
+
 func inValue(pp []Period, v int) bool {
 	for _, p := range pp {
 		if p.In(v) {
@@ -72,7 +117,7 @@ func inValue(pp []Period, v int) bool {
 }
 
 // MatchPeriods check if ref time matches any PeriodDefinition
-func MatchPeriods(pp []PeriodDefinition , ref time.Time) bool {
+func MatchPeriods(pp []PeriodDefinition, ref time.Time) bool {
 	for _, p := range pp {
 		if p.In(ref) {
 			return true
@@ -112,4 +157,3 @@ func ParsePeriod(spec string, min int, max int) ([]Period, error) {
 	}
 	return values, nil
 }
-
